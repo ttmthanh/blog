d49dad5db5afca1c645261ac9d5f31e0c391229a
badromance007 <nguyentrongtuong1993@yahoo.com>
2016-10-18 09:29:59 +0700

    add-edit-show

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,42 @@
+class ArticlesController < ApplicationController
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new;
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title,:text))
+		
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
+	def index
+		@article = Article.all
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
+end
