c1dd9b43c6b519e100c350a2db0ff0e947170bd8
badromance007 <nguyentrongtuong1993@yahoo.com>
2016-10-18 09:55:12 +0700

    delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,12 @@ class ArticlesController < ApplicationController
 			render 'edit'
 		end
 	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to welcome_index_path
+	end
 	
 	def index
 		@article = Article.all
