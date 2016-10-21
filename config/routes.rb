33fcdd04da3cd21ac00f522d10c3486d18edbb63
ttmthanh <tranthuyminhthanh@gmail.com>
2016-10-21 13:02:29 +0700

    bootstrap-simple

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   end
   root 'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # New a
+  get 'articles/new' => 'articles#new'
+
+  # list các a
+  get 'articles' => 'articles#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018030434) do
+ActiveRecord::Schema.define(version: 20161020044616) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20161018030434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.string   "tittle"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
